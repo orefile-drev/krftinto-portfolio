9795043f8b16798b1855daf9f0084ded70ff3a7d
orefile-drev <orefilebagayi46@gmail.com>
2026-09-25 17:54:29 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBWYRcpSe2HRLpm2wgjl_MPpMOR-6Ojg9c",
+  authDomain: "krftinto-gho-715f9.firebaseapp.com",
+  projectId: "krftinto-gho-715f9",
+  storageBucket: "krftinto-gho-715f9.firebasestorage.app",
+  messagingSenderId: "948792175240",
+  appId: "1:948792175240:web:129002699affdb851d0d3d",
+  measurementId: "G-NZH10TL5HS"
+};
