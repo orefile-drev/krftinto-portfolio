7703f12c33094a87b0771a9515259186fc9cffb9
orefile-drev <orefilebagayi46@gmail.com>
2026-09-25 18:08:51 +0200

    Initialize Firebase app and authentication

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,6 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBWYRcpSe2HRLpm2wgjl_MPpMOR-6Ojg9c",
   authDomain: "krftinto-gho-715f9.firebaseapp.com",
@@ -7,3 +10,8 @@ const firebaseConfig = {
   appId: "1:948792175240:web:129002699affdb851d0d3d",
   measurementId: "G-NZH10TL5HS"
 };
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { app, auth };
